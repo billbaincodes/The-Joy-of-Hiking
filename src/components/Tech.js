beab72811b9d3647c285e8faad1837b269bd817d
bill <williamandrewbain@gmail.com>
2019-03-01 21:10:57 -0700

    Update tech list, remove logo animation

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Tech = () => {
   return(
     <div>
-
+      <ul>
+        <li>JavaScript (ES6)</li>
+        <li>Node.js</li>
+        <li>React</li>
+        <li>Express</li>
+        <li>Knex</li>
+        <li>Heroku</li>
+        <li>PostgreSQL</li>
+      </ul>
     </div>
   )
 }
