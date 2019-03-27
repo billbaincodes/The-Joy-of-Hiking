1683d8f7dd56fda86ef68ae426a9cc4c246c9f45
bill <williamandrewbain@gmail.com>
2019-03-26 21:57:02 -0600

    Update home component for thumbnail layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,13 @@ class Home extends Component {
     return (
       <div className="blog">
         <Thumbnail />
+        <Thumbnail />
+        <Thumbnail />
+        <Thumbnail />
+        <Thumbnail />
+        <Thumbnail />
+        <Thumbnail />
+        
       </div>
     );
   }
