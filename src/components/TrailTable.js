1e964da51c606633e43f9542f9f601ca03412e43
bill <williamandrewbain@gmail.com>
2019-04-10 13:43:57 -0600

    Improve mobile layout

--- src/components/TrailTable.js
+++ src/components/TrailTable.js
@@ -10,36 +10,28 @@ const TrailTable = ({ trailData }) => {
             <th scope="col">Name</th>
             <th scope="col">Park</th>
             <th scope="col">Trailhead</th>
-            <th scope="col">Price</th>
+            <th className="table-price" scope="col">
+              Price
+            </th>
           </tr>
         </thead>
         <tbody>
-
-
-          {trailData ?
+          {trailData ? (
             trailData.map(trail => (
-            <tr>
-              <td>
-                <Link
-                  to={{ pathname: `/card`, state: { trailData: trail } }}
-                >
-                  {trail.name}
-                </Link>
-              </td>
-
-              <td>{trail.park_id}</td>
-              <td>{trail.trailhead_id}</td>
-              <td>{trail.price === 0 ? "Free" : `$${trail.price}`}</td>
-            </tr>
-          )) :
-
-          <p>No results found!</p>
-          
-          
-          }
-
-
-
+              <tr>
+                <td>
+                  <Link to={{ pathname: `/card`, state: { trailData: trail } }}>
+                    {trail.name}
+                  </Link>
+                </td>
+                <td>{trail.park_id}</td>
+                <td>{trail.trailhead_id}</td>
+                <td>{trail.price === 0 ? "Free" : `$${trail.price}`}</td>
+              </tr>
+            ))
+          ) : (
+            <p>No results found!</p>
+          )}
         </tbody>
       </table>
     </div>
